d7f2980925688266b98356f5a4e9994a6518ec55
Ayca Dogan <aycadgan@hotmail.com>
2019-02-03 16:35:18 +0100

    Creating a class with constructor, getter and setter methods.

---
+++ src/main/java/Price.java
@@ -0,0 +1,28 @@
+public class Price {
+
+    int firstPrice;
+    int secondPrice;
+
+    public Price(int firstPrice, int secondPrice) {
+        this.firstPrice = firstPrice;
+        this.secondPrice = secondPrice;
+    }
+
+    public int getFirstPrice() {
+        return firstPrice;
+    }
+
+    public void setFirstPrice(int firstPrice) {
+        this.firstPrice = firstPrice;
+    }
+
+    public int getSecondPrice() {
+        return secondPrice;
+    }
+
+    public void setSecondPrice(int secondPrice) {
+        this.secondPrice = secondPrice;
+    }
+
+
+}
